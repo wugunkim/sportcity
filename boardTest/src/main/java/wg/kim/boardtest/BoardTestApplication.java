5da9a6fa3136b3cdb3f0c9ceffaf58623b6f725c
wgkim <wgkim@localhost>
2022-07-30 13:42:49 +0900

    테스

---
+++ boardTest/src/main/java/wg/kim/boardtest/BoardTestApplication.java
@@ -0,0 +1,13 @@
+package wg.kim.boardtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardTestApplication.class, args);
+    }
+
+}
---
+++ boardTest/src/test/java/wg/kim/boardtest/BoardTestApplicationTests.java
@@ -0,0 +1,13 @@
+package wg.kim.boardtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
